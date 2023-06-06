89bfedbe4fa18d5b001ba6d77d4fcecb1998596b
n-brindise <n-brindise@users.noreply.github.com>
2023-06-06 15:05:50 +0200

    First finished iteration of parse_trees

--- modules/parse_trees.py
+++ modules/parse_trees.py
@@ -13,12 +13,14 @@ def index_formula(raw_str):
     # Identifies operators and atoms and indexes them.
     op_idxs = list()
     paren_map = list()
+    operator_strength = list()
+    empty_tree_nodes = list()
     
     # Add extra terminal spaces for processing purposes:
-    print('length of raw_str: ' , len(raw_str))
+    #print('length of raw_str: ' , len(raw_str))
     formula_str = ''.join([' ',raw_str, '  '])
-    print('formula_str: ', formula_str)
-    print('length of formula_str: ' , len(formula_str))
+    #print('formula_str: ', formula_str)
+    #print('length of formula_str: ' , len(formula_str))
     # Initialize parenthetical grouping level to 0:
     paren_level = 0
     # Initialize empty atom string
@@ -32,53 +34,75 @@ def index_formula(raw_str):
         if formula_str[i:i+2] == 'or' and ((formula_str[i-1] == ')' or formula_str[i-1] == ' ') and (formula_str[i+2] == '(' or formula_str[i+2] == ' ')):
             op_idxs.append('or')
             paren_map.append(paren_level)
+            operator_strength.append(0)
+            empty_tree_nodes.append(["or",list(),list()])
             i = i+2
         #'and' surrounded by ')', '(', and/or spaces:
         elif formula_str[i:i+3] == 'and' and ((formula_str[i-1] == ')' or formula_str[i-1] == ' ') and (formula_str[i+3] == '(' or formula_str[i+3] == ' ')):
             op_idxs.append('and')
             paren_map.append(paren_level)
+            operator_strength.append(0)
+            empty_tree_nodes.append(["and",list(),list()])
             i = i+3
         # '->':
         elif formula_str[i:i+2] == '->':
             op_idxs.append('->')
             paren_map.append(paren_level)
+            operator_strength.append(0)
+            empty_tree_nodes.append(["->",list(),list()])
             i = i+2
             
         # Binary operators:
         elif formula_str[i] == 'U':
             op_idxs.append('U')  
             paren_map.append(paren_level)
+            operator_strength.append(1)
+            empty_tree_nodes.append(["U",list(),list()])
             i = i+1
         elif formula_str[i] == 'R':
             op_idxs.append('R') 
             paren_map.append(paren_level)
+            operator_strength.append(1)
+            empty_tree_nodes.append(["R",list(),list()])
             i = i+1
         elif formula_str[i] == 'W':
             op_idxs.append('W') 
             paren_map.append(paren_level)
+            operator_strength.append(1)
+            empty_tree_nodes.append(["W",list(),list()])
             i = i+1
         elif formula_str[i] == 'M':
             op_idxs.append('M') 
             paren_map.append(paren_level)
+            operator_strength.append(1)
+            empty_tree_nodes.append(["M",list(),list()])
             i = i+1
             
         # Unary operators:
         elif formula_str[i] == 'X':
             op_idxs.append('X') 
             paren_map.append(paren_level)
+            operator_strength.append(2)
+            empty_tree_nodes.append(["X",list()])
             i = i+1
         elif formula_str[i] == 'F':
             op_idxs.append('F') 
             paren_map.append(paren_level)
+            operator_strength.append(2)
+            empty_tree_nodes.append(["F",list()])
             i = i+1
         elif formula_str[i] == 'G':
             op_idxs.append('G') 
             paren_map.append(paren_level)
+            operator_strength.append(2)
+            empty_tree_nodes.append(["G",list()])
             i = i+1
         # 'neg' with parentheses or spaces:
         elif formula_str[i:i+3] == 'neg' and ((formula_str[i-1] == ')' or formula_str[i-1] == ' ') and (formula_str[i+3] == '(' or formula_str[i+3] == ' ')):
             op_idxs.append('neg')
             paren_map.append(paren_level)
+            operator_strength.append(2)
+            empty_tree_nodes.append(["neg",list()])
             i = i+3
         
         # eliminate parentheses, but map them: 
@@ -98,6 +122,8 @@ def index_formula(raw_str):
                 # we've reached the end of the atom
                 op_idxs.append(atom)
                 paren_map.append(paren_level)
+                operator_strength.append(-1)
+                empty_tree_nodes.append(["AP",[atom]])
                 atom = ''
                 
             i = i+1
@@ -106,9 +132,120 @@ def index_formula(raw_str):
             # We just have an uninteresting space.
             i = i + 1
                 
-    return op_idxs, paren_map
+        index_dict = dict()
+        index_dict['op_idxs'] = op_idxs
+        index_dict['paren_map'] = paren_map
+        index_dict['operator_strength'] = operator_strength
+        index_dict['empty_tree_nodes'] = empty_tree_nodes
+        
+    return index_dict
 
+def populate_node(node_dict):
+    # Flag for if we've reached a leaf:
+    is_leaf = False
+    #print('populate_node was called!')
+    node = list()
+    
+    formula_indexed = node_dict['op_idxs']
+    paren_map = node_dict['paren_map']
+    operator_strength = node_dict['operator_strength']
+    empty_tree_nodes = node_dict['empty_tree_nodes'] 
+    
+    num_elements = len(formula_indexed)
+    #print('num_elements: ', num_elements)
+    
+    # Check if the provided argument is a leaf:
+    if len(operator_strength) == 1 and operator_strength[0] == -1:
+        is_leaf = True       
+        args_list = []
+        node = empty_tree_nodes[0]
+        #print('Leaf: ', node)
+        return node, args_list, is_leaf
+    
+    # Move through formula segment by operator strength and 
+    # parenthetical level until weakest is found:
+    
+    break_loops = False
+    #print('paren map max: ', max(paren_map))
+    for i in range(0, max(paren_map)+1): # parenthetical level
+        #print('i: ', i)
+        for j in range(0,3): # operator strength
+            #print('j: ', j)
+            for idx in range(0, num_elements): # position in indexed formula segment
+                
+                if paren_map[idx] == i and operator_strength[idx] == j:
+                    node = empty_tree_nodes[idx]
+                    break_loops = True
+                    #print('Operator was: ', formula_indexed[idx])
+                    break
+            if break_loops:
+                break
+        if break_loops:
+            break
+    
+    args_list = list()
+    left_arg = dict()
+    right_arg = dict()
+    
+    if len(node) == 3: # two-argument operator
+        
+        # Split all strings into the two arguments of the operator
+        left_arg['op_idxs'] = formula_indexed[0:idx]
+        left_arg['paren_map'] = paren_map[0:idx]
+        left_arg['operator_strength'] = operator_strength[0:idx]
+        left_arg['empty_tree_nodes'] = empty_tree_nodes[0:idx]
+        
+        right_arg['op_idxs'] = formula_indexed[idx+1:num_elements]
+        right_arg['paren_map'] = paren_map[idx+1:num_elements]
+        right_arg['operator_strength'] = operator_strength[idx+1:num_elements]
+        right_arg['empty_tree_nodes'] = empty_tree_nodes[idx+1:num_elements]
+        
+        args_list = [left_arg, right_arg]
+        
+    elif len(node) == 2: # one-argument operator            
+        
+        right_arg['op_idxs'] = formula_indexed[idx+1:num_elements]
+        right_arg['paren_map'] = paren_map[idx+1:num_elements]
+        right_arg['operator_strength'] = operator_strength[idx+1:num_elements]
+        right_arg['empty_tree_nodes'] = empty_tree_nodes[idx+1:num_elements]    
+            
+        args_list = [right_arg]   
+            
+        # if node[0] == 'AP': 
+        #     is_leaf = True       
+        #     print('Leaf: ', node[1])     
+        #     args_list = []
+            
+        # else: # not a leaf
+        #     right_arg['op_idxs'] = formula_indexed[idx+1:num_elements]
+        #     right_arg['paren_map'] = paren_map[idx+1:num_elements]
+        #     right_arg['operator_strength'] = operator_strength[idx+1:num_elements]
+        #     right_arg['empty_tree_nodes'] = empty_tree_nodes[idx+1:num_elements]    
+            
+        #     args_list = [right_arg]   
+            
+    return node, args_list, is_leaf
 
+def nest_nodes(node_dict):
+    #print('nest_nodes was called!')
+    node, args_list, is_leaf = populate_node(node_dict)
+    #print('args_list length: ', len(args_list))
+    if is_leaf:
+        #print('Leaf. Node: ', node)
+        pass
+        
+    elif len(args_list) == 1:
+        #print('args_list[0]: ', args_list[0])
+        node[1] = nest_nodes(args_list[0])
+        #print('op had one argument. Node: ', node)
+        
+    elif len(args_list) == 2:
+
+        node[1] = nest_nodes(args_list[0])
+        node[2] = nest_nodes(args_list[1])
+        #print('op had two arguments. Node: ', node)
+        
+    return node
 
 def dump_trees_json(trees, test_name='default'):
     
@@ -119,19 +256,32 @@ def dump_trees_json(trees, test_name='default'):
     with open(config_path, 'w') as f:
         json.dump(obj=trees, fp = f, indent = 4)
         
+        
 def parse_tree(formula_str):
-    tree = list()
     
-    formula_indexed, paren_map = index_formula(formula_str)
+    index_dict = index_formula(formula_str)
     
+    formula_indexed = index_dict['op_idxs']
+    paren_map = index_dict['paren_map']
+    operator_strength = index_dict['operator_strength']
+    empty_tree_nodes = index_dict['empty_tree_nodes']
     #paren_map, formula = map_parentheses(formula_clean)
     print('Tests: ')
-    print('formula length (indexed): ', len(formula_indexed))
+    #print('formula length (indexed): ', len(formula_indexed))
+    print('formula string: ', formula_str)
     print('formula_indexed: ', formula_indexed)
-    print('paren_map: ', paren_map)
+    #print('paren_map: ', paren_map)
+    #print('operator_strength: ', operator_strength)
+    #print('empty tree nodes: ', empty_tree_nodes)
+    
+    # Recursively generate trees from indexed formula
+    # (big oof)
+    full_tree = list()
+    
+    full_tree = nest_nodes(index_dict)
     
     
-    return tree
+    return full_tree
 
 
 def test_tree_parser(form_strs):
@@ -152,7 +302,7 @@ if __name__ == '__main__':
         
         'GXa -> (b or c U d)',
         'a -> bUc -> Xd',
-        'f and (a -> b U (c->d)) -> e'
+        'f and (apple ->b U (cat_box->d)) ->eggs'
     ]
     
     test_tree_parser(rule_strings)
